f11440bb8d5043007c15bb8630dbcb0c9ed08525
radu <radu.helstern@deskpro.com>
2017-06-13 17:31:21 +0100

    inject some simple html into the main deskpro window

--- src/main/javascript/App.jsx
+++ src/main/javascript/App.jsx
@@ -5,7 +5,18 @@ export default class App extends React.Component
 {
   static propTypes = { dpapp: React.PropTypes.object.isRequired };
 
+  componentDidMount() {
+    const yourMarkupStrings = [
+      `<style> #app_manipulate_dom { color: red; height:500px; z-index: 2000; position: absolute; width: 500px;} </style>`,
+      `<div id="app_manipulate_dom">Hello World!</div>`
+    ];
+    const { dpapp } = this.props;
+    dpapp.deskproWindow.insertMarkup(yourMarkupStrings);
+  }
+
   shouldComponentUpdate() { return false; }
 
-  render() { return (<div>Hello world</div>); }
+  render() {
+    return null;
+  }
 }
